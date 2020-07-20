50cfb2c6ce7ae21a760eb8ac60a4b9f0145bf38f
Anandhaselvam Kathirvelu <Anandhaselvam.Kathirvelu@sqs.com>
2020-07-20 11:52:22 +0530

    added new support

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,26 @@
 const express = require('express');
-
 const app = express();
 
+const mongoose = require('./database/mongoose');
+
+/*
+CORS - cross origin request security
+localhost:3000 - backend api
+localhost:4200 - front end
+*/
+
 app.use((req, res, next) => {
-  console.log('first middleware');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, HEAD , OPTIONS, PUT , PATCH, DELETE'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, x-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('Hello from Express');
-});
-
-var port = '3000';
-app.set('port', port);
-
-module.exports = app;
+app.use(express.json());
+app.listen(3000, () => console.log('Server Connected On Port 3000'));

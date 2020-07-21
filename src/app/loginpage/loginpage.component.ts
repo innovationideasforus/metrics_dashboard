a5c9fa89eaa1c76c6c4a666647ec3c36e87b3154
Anandhaselvam Kathirvelu <Anandhaselvam.Kathirvelu@sqs.com>
2020-07-21 21:16:37 +0530

    changes

--- src/app/loginpage/loginpage.component.ts
+++ src/app/loginpage/loginpage.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
+import { HttpResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-loginpage',
   templateUrl: './loginpage.component.html',
   styleUrls: ['./loginpage.component.css'],
 })
 export class LoginpageComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit(): void {}
-  login() {
+  /*  login() {
     this.router.navigate(['homepage']);
+  } */
+
+  onLoginButtonClicked(email: string, password: string) {
+    this.authService
+      .login(email, password)
+      .subscribe((res: HttpResponse<any>) => {
+        if (res.status === 200) {
+          // we have logged in successfully
+          this.router.navigate(['homepage']);
+        }
+        console.log(res);
+      });
   }
 }
